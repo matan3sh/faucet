62f6e0886ae8dd3ac02a5146bb526101b32d882c
Matan Shaviro <matan3sh@gmail.com>
2022-05-23 20:27:20 +0300

    first commit

---
+++ contracts/Storage.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.9.0;
+
+contract Storage {
+  uint8 public a = 7;
+  uint16 public b = 10;
+  address public c = 0x72FA9D983969590d8A95A732Df08C9512d51e1b8;
+  bool d = true;
+  uint64 public e = 15;
+
+  uint256 public f = 200;
+  uint8 public g = 40;
+  uint256 public h = 789;
+}
--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from "react";
+
+import "./App.css";
 
 function App() {
+  useEffect(() => {
+    const loadProvider = async () => {
+      // with metamask we have an access to window.ethereum and to window.web3
+      // metamask inject a global API into website
+      // this API allows websites to request access to the user's accounts, read data to blockchain, etc.
+    };
+    loadProvider();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="faucet-wrapper">
+        <div className="faucet">
+          <div className="balance-view is-size-2">
+            Current Balance: <strong>10</strong> ETH
+          </div>
+          <button
+            onClick={async () => {
+              const accounts = await window.ethereum.request({
+                method: "eth_requestAccounts",
+              });
+              console.log(accounts);
+            }}
+            className="btn mr-2">
+            Enable Etherum
+          </button>
+          <button className="btn mr-2">Donate</button>
+          <button className="btn">Withdraw</button>
+        </div>
+      </div>
+    </>
   );
 }
 
