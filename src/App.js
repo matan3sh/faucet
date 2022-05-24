7b19df5009820abe81fd4f8933d3bf659af75361
Matan Shaviro <matan3sh@gmail.com>
2022-05-24 11:21:21 +0300

    Get provider and request accounts

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import Web3 from "web3";
+import detectEthereumProvider from "@metamask/detect-provider";
 
 import "./App.css";
 
@@ -12,25 +13,17 @@ function App() {
 
   useEffect(() => {
     const loadProvider = async () => {
-      let provider = null;
+      const provider = await detectEthereumProvider();
 
-      if (window.ethereum) {
-        provider = window.ethereum;
-        try {
-          await provider.enable();
-        } catch {
-          console.error("User denied accounts access!");
-        }
-      } else if (window.web3) {
-        provider = window.web3.currentProvider;
-      } else if (!process.env.production) {
-        provider = new Web3.providers.HttpProvider("http://localhost:7545");
+      if (provider) {
+        provider.request({ method: "eth_requestAccounts" });
+        setWeb3Api({
+          web3: new Web3(provider),
+          provider,
+        });
+      } else {
+        console.error("Please install MetaMask");
       }
-
-      setWeb3Api({
-        web3: new Web3(provider),
-        provider,
-      });
     };
 
     loadProvider();
