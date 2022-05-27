f696b706a4c4a56be064161542ef7b0e661b417a
Matan Shaviro <matan3sh@gmail.com>
2022-05-27 10:41:13 +0300

    Load contract balance

--- src/App.js
+++ src/App.js
@@ -11,6 +11,8 @@ function App() {
     web3: null,
     contract: null,
   });
+
+  const [balance, setBalance] = useState(null);
   const [account, setAccount] = useState(null);
 
   useEffect(() => {
@@ -18,7 +20,6 @@ function App() {
       const provider = await detectEthereumProvider();
       const contract = await loadContract("Faucet", provider);
 
-      debugger;
       if (provider) {
         setWeb3Api({
           web3: new Web3(provider),
@@ -31,6 +32,15 @@ function App() {
     };
     loadProvider();
   }, []);
+
+  useEffect(() => {
+    const loadBalance = async () => {
+      const { web3, contract } = web3Api;
+      const balance = await web3.eth.getBalance(contract.address);
+      setBalance(web3.utils.fromWei(balance, "ether"));
+    };
+    web3Api.contract && loadBalance();
+  }, [web3Api]);
 
   useEffect(() => {
     const getAccount = async () => {
@@ -62,7 +72,7 @@ function App() {
           </div>
 
           <div className="balance-view is-size-2 my-4">
-            Current Balance: <strong>10</strong> ETH
+            Current Balance: <strong>{balance}</strong> ETH
           </div>
           <button className="button is-link mr-2">Donate</button>
           <button className="button is-primary">Withdraw</button>
