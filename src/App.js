79de1e1a9b2f85ef091ac387d97256edecbaa4b9
Matan Shaviro <matan3sh@gmail.com>
2022-05-27 09:31:22 +0300

    Re migrate contracts, Get deployed contract

--- src/App.js
+++ src/App.js
@@ -16,8 +16,9 @@ function App() {
   useEffect(() => {
     const loadProvider = async () => {
       const provider = await detectEthereumProvider();
-      const contract = await loadContract("Faucet");
+      const contract = await loadContract("Faucet", provider);
 
+      debugger;
       if (provider) {
         setWeb3Api({
           web3: new Web3(provider),
--- src/utils/load-contract.js
+++ src/utils/load-contract.js
@@ -1,8 +1,13 @@
 import contract from "@truffle/contract";
 
-export const loadContract = async (name) => {
+export const loadContract = async (name, provider) => {
   const res = await fetch(`/contracts/${name}.json`);
-  const artifact = await res.json();
+  const Artifact = await res.json();
 
-  return contract(artifact);
+  const _contract = contract(Artifact);
+  _contract.setProvider(provider);
+
+  const deployedContract = await _contract.deployed();
+
+  return deployedContract;
 };
